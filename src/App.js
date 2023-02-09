32ac890a3492c3a59b1bba195eddd95f8ed900bf
nikamwi <114087929+nikamwi@users.noreply.github.com>
2023-02-09 07:10:08 -0800

    ar gamoaq ekranze

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const generateUsers = () => {
+  return [
+    {name: 'name1' , lname:'lname1', age: 12, id:1},
+    {name: 'name2' , lname:'lname2', age: 21, id:2},
+    {name: 'name3' , lname:'lname3', age: 23, id:3},
+    {name: 'name4' , lname:'lname4', age: 65, id:4},
+    {name: 'name5' , lname:'lname5', age: 23, id:5},
+    {name: 'name6' , lname:'lname6', age: 13, id:6},
+    {name: 'name7' , lname:'lname7', age: 52, id:7},
+    {name: 'name8' , lname:'lname8', age: 13, id:8},
+    {name: 'name9' , lname:'lname9', age: 54, id:9},
+    {name: 'name10' , lname:'lname10', age: 56, id:10}
+  ]
+}
+
 function App() {
+  
+
+  const [users,setUsers] = useState([]);
+   
+  useEffect(() => {
+    setUsers(generateUsers());
+  }, []);
+
+  useEffect(() => {
+    document.title =" ${users.length} users left ";
+  }, [users])
+
+
+  const remove = () =>{
+    setUsers((prevUsers) => {
+      const index = Math.floor(Math.random() * prevUsers.length);
+      const newUsers = prevUsers.filter((...  ind) => ind !== index);
+      return newUsers
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={remove}>remove users</button>  
+      {users.map(({id, name, lname, age}) => {
+         <h1 key={id}>{name} {lname} {age}</h1>
+      })}
     </div>
   );
 }
 
 export default App;
+// ekranze ar gamoaq
